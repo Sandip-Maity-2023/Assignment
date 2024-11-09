fd99215607131474a86b1a6e14c7470b445c9805
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-11-09 20:06:10 +0530

    Assignment

---
+++ cmake-build-debug/CMakeFiles/Assignment.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Assignment.exe"
+  "Assignment.exe.manifest"
+  "Assignment.pdb"
+  "CMakeFiles/Assignment.dir/main.cpp.obj"
+  "CMakeFiles/Assignment.dir/main.cpp.obj.d"
+  "libAssignment.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
